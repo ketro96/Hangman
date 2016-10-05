34514fe1db31503788aa8ee6d8e8e18fe44036ff
Adrian Werner <a.m.werner65@gmail.com>
2016-10-05 14:54:41 +0200

    Chat funktionen geändert

--- game.h
+++ game.h
@@ -19,6 +19,8 @@ public slots:
 private slots:
     void on_btnSend_clicked();
     void newServerInfo(QString ipAdress, QString port);
+    void on_edtChat_returnPressed();
+
 signals:
     void sendMessage(QString message);
 private:
